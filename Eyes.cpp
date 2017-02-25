c40e83525ef8eb4259cd61116991881e58feda2d
Alex Hamilton <alex.hamilton@ou.edu>
2017-02-25 00:13:41 -0600

    Ugly vector conversion stuff. Please don't judge me.

--- Eyes.cpp
+++ Eyes.cpp
@@ -29,7 +29,15 @@ unsigned Eyes::filteredNumberOfCircles(cv::vector<cv::Vec3f> circles) {
 }
 
 unsigned Eyes::numberOfDots(void) { // An unsigned integer makes more sense here than does a signed one, but error reporting becomes harder
-	cv::Mat image = cv::imdecode(Camera::takePicture(), 0);
+	short thing[160][120] = Camera::takePicture();
+	std::vector<std::vector<short>> img;
+	for(int i = 0; i < 160; i++) {
+		img.push_back(std::vector<short>());
+		for(int j = 0; j < 120; j++) {
+			img.at(i).push_back(thing[i][j]);
+		}
+	}
+	cv::Mat image = cv::imdecode(img, 0);
 	cv::vector<cv::Vec3f> circles; // HoughCircles will assign to this values of (x, y, r)
 	cv::GaussianBlur(image, image, cv::Size(KERNEL_WIDTH, KERNEL_WIDTH), 0, 0); // (input, output, kernel size, sigma_x, sigma_y)
 	cv::HoughCircles(image, circles, CV_HOUGH_GRADIENT, 1, MINIMUM_OFFSET, UPPER_THRESHOLD, LOWER_THRESHOLD, MINIMUM_RADIUS, MAXIMUM_RADIUS);
